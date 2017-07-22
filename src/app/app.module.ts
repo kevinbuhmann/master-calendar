c83150702db60f61bbe3ff551a9871d8064aa3e1
Kevin Phelps <KevinPhelps11@gmail.com>
2017-07-22 09:42:13 -0500

    feat: add firebase setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from 'angularfire2';
 
+import { environment } from './../environments/environment';
 import { AppMaterialModule } from './app-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirebaseService } from './services/firebase.service';
 
 import 'hammerjs';
 
@@ -15,10 +18,13 @@ import 'hammerjs';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
     AppMaterialModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    FirebaseService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.common.ts
+++ src/environments/environment.common.ts
@@ -1,6 +1,16 @@
 const node = typeof window === 'undefined';
 
+const firebaseConfig = {
+  apiKey: 'AIzaSyCeqGlAHuFRBXvarjoAzr9ifIBqPibbM5c',
+  authDomain: 'master-calendar-54d80.firebaseapp.com',
+  databaseURL: 'https://master-calendar-54d80.firebaseio.com',
+  projectId: 'master-calendar-54d80',
+  storageBucket: 'master-calendar-54d80.appspot.com',
+  messagingSenderId: '75912964717'
+};
+
 export const commonEnvironment = {
   node,
-  browser: node === false
+  browser: node === false,
+  firebaseConfig
 };
