5aab7cb77dbadc25242765de29fd129adb41dbe8
Kevin Phelps <KevinPhelps11@gmail.com>
2017-07-22 09:58:55 -0500

    refactor: move angular material import around

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from 'angularfire2';
 
 import { environment } from './../environments/environment';
-import { AppMaterialModule } from './app-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirebaseService } from './services/firebase.service';
-
-import 'hammerjs';
+import { AppMaterialModule } from './shared/app-material.module';
 
 @NgModule({
   declarations: [
